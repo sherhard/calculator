2417da074f71e8cf51a7437ab1795b850e1becc5
sherhard <goeasysher@gmail.com>
2023-02-05 10:14:47 +0800

    Add existing file

--- calcul.js
+++ calcul.js
@@ -4,6 +4,7 @@ let buttonPlus=document.getElementById('buttonPlus');
 let buttonMinus=document.getElementById('buttonMinus');
 let buttonMultiply=document.getElementById('buttonMultiply');
 let buttonDevide=document.getElementById('buttonDevide');
+let result=document.getElementById('result');
 
 let number1=document.getElementById('number1');
 let number2=document.getElementById('number2');
@@ -15,27 +16,27 @@ let number2=document.getElementById('number2');
 function onButtonClickPlus(){
 	let value1=Number(number1.value);
 	let value2=Number(number2.value);
-	let result=value1+value2;
-	console.log(result);
+	 result.value=value1+value2;
+	
 
 }
 function onButtonClickMinus(){
 	let value1=Number(number1.value);
 	let value2=Number(number2.value);
-	let result=value1-value2;
-	console.log(result);
+	 result.value=value1-value2;
+	
 }
 function onButtonClickMultiply(){
 	let value1=Number(number1.value);
 	let value2=Number(number2.value);
-	let result=value1*value2;
-	console.log(result);
+	 result.value=value1*value2;
+	
 }
 function onButtonClickDevide(){
 	let value1=Number(number1.value);
 	let value2=Number(number2.value);
-	let result=value1/value2;
-	console.log(result);
+	 result.value=value1/value2;
+	
 }
 
 buttonPlus.addEventListener('click',onButtonClickPlus); 
