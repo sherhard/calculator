f4ecb82273c9aacc8fd70fb4a64f0962c975ea3b
sherhard <goeasysher@gmail.com>
2023-02-14 13:39:25 +0800

    add if else

--- calcul.js
+++ calcul.js
@@ -9,46 +9,44 @@ let result=document.getElementById('result');
 let number1=document.getElementById('number1');
 let number2=document.getElementById('number2');
 
+function getNumber1(){
 
-let allButton=document.getElementsByClassName('bntNumber');
-	
-	for(let i=0; i<allButton.length; i++){
-		allButton[i].addEventListener('click',btn);
-	}
-
-	
-
-function btn(){
-	number1.value=this.value;
-	number2.value=this.value;
+	return Number(number1.value);
 }
 
+function getNumber2(){
 
+	return Number(number2.value);
+}
 
+function makeOperation(operationCode){
+	if (operationCode === '+'){
+	 result.value=getNumber1()+getNumber2();
+	} else if (operationCode === '-'){
+		result.value=getNumber1()-getNumber2();
+	} else if (operationCode === '*'){
+		result.value=getNumber1()*getNumber2();
+	} else if (operationCode === '/'){
+		result.value=getNumber1()/getNumber2();
+	}
+}
 
 function onButtonClickPlus(){
-	let value1=Number(number1.value);
-	let value2=Number(number2.value);
-	 result.value=value1+value2;
-	
 
+	makeOperation('+');
 }
+
 function onButtonClickMinus(){
-	let value1=Number(number1.value);
-	let value2=Number(number2.value);
-	 result.value=value1-value2;
-	
+
+	 makeOperation('-');
 }
 function onButtonClickMultiply(){
-	let value1=Number(number1.value);
-	let value2=Number(number2.value);
-	 result.value=value1*value2;
-	
+
+ 	 makeOperation('*');	
 }
 function onButtonClickDevide(){
-	let value1=Number(number1.value);
-	let value2=Number(number2.value);
-	 result.value=value1/value2;
+
+	makeOperation('/');
 	
 }
 
